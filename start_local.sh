4cff135e9ff16d60c86374e9ca4e25ac561a4cae
berrydatadev <info@berrydata.co>
2021-01-25 16:49:13 +0800

    fix local config path

--- start_local.sh
+++ start_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nohup ./berriot --config=test_cfgs/config1.json mine >> logs/1.log &
-nohup ./berriot --config=test_cfgs/config2.json mine >> logs/2.log &
-nohup ./berriot --config=test_cfgs/config3.json mine >> logs/3.log &
-nohup ./berriot --config=test_cfgs/config4.json mine >> logs/4.log &
-nohup ./berriot --config=test_cfgs/config5.json mine >> logs/5.log &
+nohup ./berriot --config=local/config1.json mine >> logs/1.log &
+nohup ./berriot --config=local/config2.json mine >> logs/2.log &
+nohup ./berriot --config=local/config3.json mine >> logs/3.log &
+nohup ./berriot --config=local/config4.json mine >> logs/4.log &
+nohup ./berriot --config=local/config5.json mine >> logs/5.log &
